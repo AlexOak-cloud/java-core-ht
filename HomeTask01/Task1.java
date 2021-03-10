dc989ee3ddc6e178309ec38201734ae21c8e76a6
Александр Романович <64036676+AlexOak-cloud@users.noreply.github.com>
2021-03-10 21:42:08 +0300

    new

---
+++ HomeTask01/Task1.java
@@ -0,0 +1,12 @@
+package HomeTask01;
+
+//     1 Исправить код, чтобы программа вывела ​IT College!​.
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("IT College!");
+    }
+}
+
